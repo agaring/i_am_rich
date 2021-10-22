18b88dcab203b7001007fa0aa233eca737c07f39
Arnaud Garing <arnaud.garing67@gmail.com>
2021-10-22 15:44:19 +0200

    Start of the project.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Main is the starting point for hte flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
